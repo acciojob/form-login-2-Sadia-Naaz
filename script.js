33f0543ac5be9dd9cc236c7aff73afb4b5ff1180
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-16 22:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+	let myForm=document.getElementById("myForm");
+   myForm.addEventListener('click',postFill);
+    
+   function postFill(event){
+	   alert(myForm.value);
+   }
