e558e2d2ee6c5c2c6e2af9ccfb4a7a47e77e7aec
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-16 23:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 //your JS code here. If required.
-	let myForm=document.getElementById("myForm");
-   myForm.addEventListener('click',postFill);
-    
-   function postFill(event){
-	   alert(myForm.value);
-   }
+let myForm=document.getElementById("myForm");
+function  showAlert(){
+	
+ 
+let first=document.getElementById("first").value;
+let last=document.getElementById("Last").value;
+let Number=document.getElementById("Phone Number").value;
+let email=document.getElementById("Email ID").value;
+let message = "First Name:"+first+"\n"+"Last Name:"+last+"\n"+"Phone Number:"+Number+"\n"+"Email ID:"+email;
+	 alert(message);
+ } 
+   
