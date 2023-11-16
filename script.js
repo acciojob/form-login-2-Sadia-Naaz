1cdb8047573d5a04423b52b7809438db02151411
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-16 23:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let myForm=document.getElementById("myForm");
 function  showAlert(){
 	
  
-let first=document.getElementById("first").value;
-let last=document.getElementById("Last").value;
+let first=document.getElementById("First Name").value;
+let last=document.getElementById("Last Name").value;
 let Number=document.getElementById("Phone Number").value;
 let email=document.getElementById("Email ID").value;
-let message = "First Name:"+first+"\n"+"Last Name:"+last+"\n"+"Phone Number:"+Number+"\n"+"Email ID:"+email;
+let message = "First Name: "+first+"\n"+"Last Name: "+last+"\n"+"Phone Number: "+Number+"\n"+"Email ID: "+email;
 	 alert(message);
  } 
    
